b3ed062053207be50e5c3159589cd87f4567b090
longday5 <1353772131@qq.com>
2019-10-27 13:50:31 +0800

    修改了一个用不到的import

--- Memorize_helper/src/controller/main/ShowVocabularyListController.java
+++ Memorize_helper/src/controller/main/ShowVocabularyListController.java
@@ -5,7 +5,6 @@
  */
 package controller.main;
 
-import com.sun.xml.internal.fastinfoset.vocab.Vocabulary;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
