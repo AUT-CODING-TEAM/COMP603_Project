60e53d1b1df6380862894164b93ce22dd86ac8e6
longday5 <1353772131@qq.com>
2019-12-08 22:17:05 +0800

    用户可正常注册登录

--- Memorize_helper/src/controller/myPlan/MakePlanController.java
+++ Memorize_helper/src/controller/myPlan/MakePlanController.java
@@ -8,6 +8,7 @@ package controller.myPlan;
 import controller.interfaces.UserController;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 import model.User;
 import view.main.MainView;
@@ -21,10 +22,12 @@ import view.planList.CreatePlanPanel;
 public class MakePlanController implements ActionListener{
     private User user;
     private CreatePlanPanel createPlanPanel;
+    private JFrame selectedPlanFrame;
     
-    public MakePlanController(User user, CreatePlanPanel createPlanPanel){
+    public MakePlanController(User user, CreatePlanPanel createPlanPanel, JFrame selectedPlanFrame){
         this.user = user;
         this.createPlanPanel = createPlanPanel;
+        this.selectedPlanFrame = selectedPlanFrame;
     }
 
     @Override
@@ -41,6 +44,7 @@ public class MakePlanController implements ActionListener{
                 System.out.println(Integer.parseInt(createPlanPanel.getQuantity().split("词")[0]));
                 new UserController().activateStudyPlanByNum(user, createPlanPanel.getSelectedPlan().getStudyPlanName(), Integer.parseInt(createPlanPanel.getQuantity().split("词")[0]));
             }
+            selectedPlanFrame.dispose();
             new MainView(user);
         }
     }
