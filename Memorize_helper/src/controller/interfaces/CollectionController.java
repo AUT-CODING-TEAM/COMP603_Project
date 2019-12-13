1c452dd01036b52ca89f037e86d4085f87a86538
KrisCris <hi.imhpc@outlook.com>
2019-12-13 23:46:13 +0800

    getCollectedWord 修复。。

--- Memorize_helper/src/controller/interfaces/CollectionController.java
+++ Memorize_helper/src/controller/interfaces/CollectionController.java
@@ -102,17 +102,28 @@ public class CollectionController {
         Database db = Database.getInstance();
         String userId = user.getID() + "";
         String wordSource = user.getCurrentStudyPlan().getStudyPlanName();
-        ResultSet rs =db.get(
-                "COLLECTION",
-                new String[]{
-                    "USER_ID",
-                    "WORD_SOURCE"
-                },
-                new String[]{
-                    userId,
-                    wordSource
-                }
-        );
+//        ResultSet rs =db.get(
+//                "COLLECTION",
+//                new String[]{
+//                    "USER_ID",
+//                    "WORD_SOURCE"
+//                },
+//                new String[]{
+//                    userId,
+//                    wordSource
+//                }
+//        );
+        StringBuilder sb = new StringBuilder();
+        sb.append("select ")
+                .append(wordSource)
+                .append(".* from COLLECTION,")
+                .append(wordSource)
+                .append(" where COLLECTION.WORD_SOURCE = ? and COLLECTION.USER_ID = ? and ")
+                .append(wordSource)
+                .append(".ID = int(COLLECTION.WORD_ID)");
+        
+        ResultSet rs = db.prepare(sb.toString(), wordSource, userId);
+        
         
         while(rs.next()){
             words.add(new Word(
